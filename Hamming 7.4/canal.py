cf6929b3b009bb7e875083b46fce1b951b55df17
gabilima <gabrielabarrosolimagbl@gmail.com>
2017-11-26 23:23:00 -0200

    ,,,

--- Hamming 7.4/canal.py
+++ Hamming 7.4/canal.py
@@ -4,32 +4,34 @@ arq = open("codificado.txt", "r")
 
 saida = open("recebido.txt", "w")
 
-p = float(input("Qual a probabilidade de erro do canal? "))
+prob = float(input("Qual a probabilidade de erro do canal? "))
 
 
-if p > 0.5:
-	p = float(input("Entrada invalida, o parametro deve estar entre 0 e 0.5. Digite novamente: "))
+if prob > 0.5:
+	prob = float(input("Entrada invalida, o parametro deve estar entre 0 e 0.5. Digite novamente: "))
 
-X = arq.readline()
+qtd = arq.readline()
+saida.write(qtd)
+for i in range(int(qtd)):
+	vetorEntrada = arq.readline()
+	vetorEntrada = vetorEntrada.rstrip('\n')
 
-X = X.rstrip('\n')
+	vetorSaida = ""
 
-Y = []
-
-for x in X:
-	
-	mudar = random.randint(0,100)
-
-	if mudar < 100*p:
-		if x == "1":
-			Y.append("0")
+	for x in vetorEntrada:
 		
-		elif x == "0": 
-			Y.append("1")
-	
-	else:
-		Y.append(x)
+		mudar = random.randint(0,100)
 
-saida.writelines(Y)
+		if mudar < 100*prob:
+			if x == "1":
+				vetorSaida = vetorSaida + "0"
+			
+			elif x == "0": 
+				vetorSaida = vetorSaida + "1"
+		
+		else:
+			vetorSaida = vetorSaida + x
+
+	saida.write(str(vetorSaida) + "\n")
 saida.close()
 arq.close()
--- Hamming 7.4/decodificador.py
+++ Hamming 7.4/decodificador.py
@@ -7,40 +7,44 @@ def mod2(x):
 arq = open("recebido.txt", "r")
 saida = open("decodificado.txt", "w")
 
-stringEntrada = arq.read()
-stringEntrada = stringEntrada.rstrip("\n")
+qtd = arq.readline()
 
-vetorEntrada = []
+for i in range(int(qtd)):
+	stringEntrada = arq.readline()
+	stringEntrada = stringEntrada.rstrip("\n")
+	vetorEntrada = []
 
-for i in stringEntrada:
-	vetorEntrada.append(int(i))
+	for j in stringEntrada:
+		vetorEntrada.append(int(j))
 
-matrixCol = np.matrix(vetorEntrada).getT()
+	matrixCol = np.matrix(vetorEntrada).getT()
+	
+	H = [[1, 1, 1, 0, 1, 0, 0], [1, 0, 1, 1, 0, 1, 0], [1, 1, 0, 1, 0, 0, 1]] 
 
-H = [[1, 1, 1, 0, 1, 0, 0], [1, 0, 1, 1, 0, 1, 0], [1, 1, 0, 1, 0, 0, 1]] 
+	H = np.matrix(H)
+	M = H*matrixCol
 
-H = np.matrix(H)
-M = H*matrixCol
+	vecFunc = np.vectorize(mod2)
+	vectorError = vecFunc(M)
+	vectorError = str(vectorError)
+	stringError = ""
 
-vecFunc = np.vectorize(mod2)
-vectorError = vecFunc(M)
-vectorError = str(vectorError)
-stringError = ""
+	for k in range(3):
+		stringError = stringError + vectorError[2 + 5*k]
 
-for i in range(3):
-	stringError = stringError + vectorError[2 + 5*i]
+	cases = {"000":0, "001": 5, "010":6, "011": 4, "100":7, "101":2, "110":3, "111":1}
+	index = cases.get(stringError, 'default')
 
-cases = {"000":0, "001": 5, "010":6, "011": 4, "100":7, "101":2, "110":3, "111":1}
-index = cases.get(stringError, 'default')
+	if index != 0:
+		index = index - 1
+		vetorEntrada[index] = (vetorEntrada[index]+1)%2
 
-if index != 0:
-	index = index - 1
-	vetorEntrada[index] = (vetorEntrada[index]+1)%2
+	decodificado = vetorEntrada[:4]
 
-decodificado = vetorEntrada[:4]
+	for i in decodificado:
+		saida.write(str(int(i)))
 
-for i in decodificado:
-	saida.write(str(int(i)))
+	saida.write("\n")
 
 arq.close()
 saida.close()
